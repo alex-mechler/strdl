77d0f62ad56ea8700c551cfa9ce301b78f21fd36
Alexander Mechler <axm8289@rit.edu>
2016-10-01 21:29:08 -0400

    Updated main function

--- src/strdl.py
+++ src/strdl.py
@@ -7,7 +7,7 @@ def main():
     parser.add_argument('filename', type=argparse.FileType('r'), nargs='+', help='The list of files to generate strdl documentation for')
     args = parser.parse_args()
     for file in args.filename:
-        strdl_parser.parse(file)
+        print(strdl_parser.parse(file))
 
 
 if __name__ == '__main__':
