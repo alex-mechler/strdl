3cc5faf72ee2ce8db1ee667adc421a643fb597c3
Alexander Mechler <axm8289@rit.edu>
2016-10-01 23:23:46 -0400

    Added example

---
+++ examples/strdl_example.py
@@ -0,0 +1,18 @@
+"""This is an example of strdl documentation"""
+
+def function(parameter):
+    """
+    This is what a function looks like in strdl documentation
+    :param parameter: This is what a parameter looks like in strdl documentation
+    :return: This is what a return looks like in strdl documentation
+    """
+    pass
+
+def function2(parameter1, parameter2):
+    """
+    This is another function example
+    :param parameter1: This is what the first parameter looks like
+    :param parameter2: And this is the second
+    :return: Another return value
+    """
+    pass
