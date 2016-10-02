c17bc74832218dc4bdcb2cbad061cd0da1bc2892
Alexander Mechler <axm8289@rit.edu>
2016-10-01 21:28:46 -0400

    Finished parsing of functions

--- src/strdl_parser.py
+++ src/strdl_parser.py
@@ -1,10 +1,31 @@
 import inspect
 import importlib
+import strdl_struct
 
 
 def parse(file):
-    module = importlib.import_module(file.name.strip('.py'))
-    print(module.__doc__)
+    filename = file.name.strip('.py')
+    module = importlib.import_module(filename)
+    module_doc = module.__doc__
     function_list = inspect.getmembers(module, inspect.isfunction)
+    functs = []
     for function in function_list:
-        print(getattr(module, function[0]).__doc__)
+        funct_string = getattr(module, function[0]).__doc__
+        params = get_params(funct_string)
+        functs.append(strdl_struct.strdl_method(function[0], params))
+    return strdl_struct.strdl_struct(filename, module_doc, functs)
+
+
+def get_params(funct_string) -> list:
+        funct_string.strip()
+        functs = []
+        for line in funct_string.split('\n'):
+            line.strip()
+            if 'param' in line:
+                split = line.split(' ')
+                print(split)
+                docs = ''
+                for i in range(6, len(split)):
+                    docs += split[i] + ' '
+                functs.append(strdl_struct.strdl_param(split[5].strip(':'), docs))
+        return functs
